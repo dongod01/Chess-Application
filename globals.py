abbd6daf40c606436e2ce006dd5ffbe27b34eb31
girinath <girinath2452001@gmail.com>
2021-04-20 22:14:26 +0530

    Multiple files

---
+++ globals.py
@@ -0,0 +1,21 @@
+window = None
+lock = None
+board = None
+
+button_list = []
+chess_list = []
+
+color_val = None
+my_socket = None
+
+name1=""
+name2=""
+
+name_label3 = None
+name_label4 = None
+
+newp='t'
+
+#Even_clicks checker
+x = True
+prev = -1
